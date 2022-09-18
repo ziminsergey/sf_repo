f7a0c711b5c80cddd0b75116244eaa3cbeb74418
Zimin Sergey <whataboutzi@gmail.com>
2022-09-18 23:23:12 +0300

    add tf project for SF 5.5

---
+++ 5/final/outputs.tf
@@ -0,0 +1,15 @@
+output "internal_ip_address_vm_1" {
+  value = module.ya_instance_1.internal_ip_address_vm
+}
+
+output "external_ip_address_vm_1" {
+  value = module.ya_instance_1.external_ip_address_vm
+}
+
+output "internal_ip_address_vm_2" {
+  value = module.ya_instance_2.internal_ip_address_vm
+}
+
+output "external_ip_address_vm_2" {
+  value = module.ya_instance_2.external_ip_address_vm
+}
